ccb4aa213f04bd966821b7106c0ba0437914d82f
Максим <116649316+ParamonovMO@users.noreply.github.com>
2023-11-28 14:30:11 +0500

    Update

--- 4.05 Заполнение полей с помощью метода send_keys.py
+++ 4.05 Заполнение полей с помощью метода send_keys.py
@@ -2,12 +2,12 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-link = 'https://www.saucedemo.com/'
-driver.get(link)
-
-username = driver.find_element(By.ID, "user-name") #поиск по ID
-username.send_keys("standard_user") #Отправка данных в поле
-
-time.sleep(3)
-driver.close()
+try:
+  driver = webdriver.Chrome()
+  link = 'https://www.saucedemo.com/'
+  driver.get(link)
+  username = driver.find_element(By.ID, "user-name") #поиск по ID
+  username.send_keys("standard_user") #Отправка данных в поле
+finally:
+  time.sleep(3)
+  driver.close()
