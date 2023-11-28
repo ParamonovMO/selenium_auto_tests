fbe2d67d166d117ecaa2dde3e0f97a3e7bfb411e
Максим <116649316+ParamonovMO@users.noreply.github.com>
2023-11-28 14:29:05 +0500

    Update

--- 4.04 Открытие браузера с помощью Selenium.py
+++ 4.04 Открытие браузера с помощью Selenium.py
@@ -1,10 +1,11 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome() #определяем драйвер хром
-link = 'https://www.saucedemo.com/' #присваиваем ссылку
-try: 
+
+try:
+  driver = webdriver.Chrome() #определяем драйвер хром
+  link = 'https://www.saucedemo.com/' #присваиваем ссылку
   driver.get(link) #переход по ссылке
 finally:
   time.sleep(3) #время ожидания 
-  driver.close() #закрытие брайзера
+  driver.close() #закрытие браузера
