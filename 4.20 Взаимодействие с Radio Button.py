f0ea067325f326b65ac3d17f481bac6cf49ce678
Максим <116649316+ParamonovMO@users.noreply.github.com>
2023-11-28 16:57:44 +0500

    Update

--- 4.20 Взаимодействие с Radio Button.py
+++ 4.20 Взаимодействие с Radio Button.py
@@ -1,20 +1,15 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-g = Service()
-driver = webdriver.Chrome(options=options, service=g)
-base_url = 'https://demoqa.com/radio-button'
-driver.get(base_url)
-driver.maximize_window()
+try:
+  link = 'https://demoqa.com/radio-button'
+  driver.get(link)
 
-radio_button = driver.find_element(By.CSS_SELECTOR, "[for='yesRadio']")
-radio_button.click()
-print("Click Radio Button")
+  radio_button = driver.find_element(By.CSS_SELECTOR, "[for='yesRadio']")
+  radio_button.click()
+  print("Click Radio Button")
 
-
-time.sleep(1)
-driver.close()
+finally:
+  time.sleep(1)
+  driver.close()
